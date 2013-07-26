172bfc77346a1a75fb9d43e7aa5973cafe9425a9
Chris Beauchamp <cjbeauchamp@gmail.com>
2013-07-26 10:54:33 -0700

    updated licenses

--- KTUI/KTLoader/KTLoaderProgressIndicator.m
+++ KTUI/KTLoader/KTLoaderProgressIndicator.m
@@ -1,24 +1,31 @@
 //
-//  KTLoaderProgressIndicator.m
-//  MyPix
 //
-//  Created by Chris on 7/25/13.
-//  Copyright (c) 2013 Kii. All rights reserved.
+// Copyright 2013 Kii Corporation
+// http://kii.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 //
 
 #import "KTLoaderProgressIndicator.h"
 
 @implementation KTLoaderProgressIndicator
 
-@synthesize currentValue,lineColor,fillColor;
-
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
-        lineColor = [UIColor whiteColor];
-        fillColor = [UIColor whiteColor];
-        currentValue = 0.0;
+        _progress = 0.0;
     }
     return self;
 }
@@ -34,13 +41,13 @@
     // draw circle stroke
     CGContextSetLineWidth(contextRef, 2.0);
     CGContextAddArc(contextRef, center.x, center.y, radius, 0, 2*M_PI, 0);
-    CGContextSetStrokeColorWithColor(contextRef, [lineColor CGColor]);
+    CGContextSetStrokeColorWithColor(contextRef, [UIColor whiteColor].CGColor);
     CGContextStrokePath(contextRef);
     
     // draw filling starting at 0%.
     // must subtract pi/2 to transform starting position to top of circle
-    float finalDegree = currentValue*2*M_PI - M_PI/2;
-    CGContextSetFillColorWithColor(contextRef, [fillColor CGColor]);
+    float finalDegree = _progress*2*M_PI - M_PI/2;
+    CGContextSetFillColorWithColor(contextRef, [UIColor whiteColor].CGColor);
     CGContextMoveToPoint(contextRef, center.x, center.y);
     CGContextAddArc(contextRef, center.x, center.y, radius, -M_PI/2, finalDegree, 0);
     CGContextClosePath(contextRef);
@@ -48,20 +55,12 @@
 }
 
 - (void)setProgress:(double)value {
-    currentValue = value;
-    if (currentValue <= 1.0) {
+    _progress = value;
+    NSLog(@"Value: %g", value);
+    NSLog(@"Progress: %g", value);
+    if (_progress <= 1.0) {
         [self setNeedsDisplay];
     }
-}
-
-- (void)setLineColor:(UIColor *)newLineColor {
-    lineColor = newLineColor;
-    [self setNeedsDisplay];
-}
-
-- (void)setFillColor:(UIColor *)newFillColor {
-    fillColor = newFillColor;
-    [self setNeedsDisplay];
 }
 
 @end
